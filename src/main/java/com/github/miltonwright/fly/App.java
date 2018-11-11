06f9253908733d1b025235b7fef53a41d5e974c3
Milton Wright <miltonwright@example.com>
2018-11-11 20:04:39 +0100

    Add parsing of XML and TSV formatting

--- src/main/java/com/github/miltonwright/fly/App.java
+++ src/main/java/com/github/miltonwright/fly/App.java
@@ -1,24 +1,45 @@
 package com.github.miltonwright.fly;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Strings;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
 
 public class App {
+
+    private static final ZoneId TIME_ZONE_NORWAY = ZoneId.of("Europe/Oslo");
 
     private static final CacheLoader<String, List<Flight>> FLIGHT_LOADER = new CacheLoader<String, List<Flight>>() {
         @Override
@@ -43,7 +64,7 @@ public class App {
             httpResponse.setContentType("text/plain");
             try (PrintWriter out = new PrintWriter(httpResponse.getOutputStream())) {
                 for (Flight f : flightCache.getUnchecked("")) {
-                    out.println(f.flightId);
+                    out.println(Joiner.on("\t").join(ImmutableList.of(f.flightId, f.airline, f.domInt, f.scheduleTime, f.airport, MoreObjects.firstNonNull(f.gate, ""), f.getStatusString())));
                 }
             } finally {
                 request.setHandled(true);
@@ -52,7 +73,75 @@ public class App {
     }
 
     public static class Flight {
-        String flightId;
+        final String flightId;
+        final String airline;
+        final String domInt;
+        final Instant scheduleTime;
+        final String airport;
+        final String gate;
+        final String statusCode;
+        final Instant statusTime;
+
+        Flight(Map<String, String> params) {
+            flightId = params.get("flight_id");
+            airline = params.get("airline");
+            domInt = params.get("dom_int");
+            scheduleTime = Strings.isNullOrEmpty(params.get("schedule_time")) ? null : Instant.parse(params.get("schedule_time"));
+            airport = params.get("airport");
+            gate = params.get("gate");
+            statusCode = params.get("status->code");
+            statusTime = Strings.isNullOrEmpty(params.get("status->time")) ? null : Instant.parse(params.get("status->time"));
+        }
+
+        String getStatusString() {
+            if (statusCode == null) {
+                return "";
+            }
+
+            // Instead, use https://flydata.avinor.no/flightStatuses.asp?code=XX
+            // Also, convert to human friendly time format if this should be read by humans
+            switch(statusCode) {
+                case "A": return "Arrived at " + statusTime.atZone(TIME_ZONE_NORWAY).format(DateTimeFormatter.ISO_LOCAL_TIME);
+                case "C": return "Cancelled";
+                case "D": return "Departed at " + statusTime.atZone(TIME_ZONE_NORWAY).format(DateTimeFormatter.ISO_LOCAL_TIME);
+                case "E": return "New time";
+                case "N": return "New info";
+                default: return "";
+            }
+        }
+    }
+
+    /**
+     * Quick and dirty way to convert InputStream with xml from
+     * https://flydata.avinor.no/XmlFeed.asp to list of flights with attributes.
+     */
+    public static List<Flight> unXml(InputStream in) throws ParserConfigurationException, SAXException, IOException {
+        List<Map<String, String>> result = new ArrayList<>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(in);
+        NodeList flights = document.getElementsByTagName("flight");
+        for (int i = 0; i < flights.getLength(); i++) {
+            NodeList list = flights.item(i).getChildNodes();
+            Map<String, String> values = new HashMap<>();
+            for (int j = 0; j < list.getLength(); j++) {
+                Node node = list.item(j);
+                if (node.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
+                }
+                Element elem = (Element) node;
+                if ("status".equals(elem.getTagName())) {
+                    values.put("status->code", elem.getAttribute("code"));
+                    values.put("status->time", elem.getAttribute("time"));
+                } else {
+                    values.put(elem.getTagName(), elem.getTextContent());
+                }
+            }
+            result.add(values);
+        }
+
+        return result.stream().map(s -> new Flight(s)).collect(Collectors.toList());
     }
 
     public static Server createServer(int port, CacheLoader<String, List<Flight>> cacheLoader) throws Exception {
--- src/test/java/com/github/miltonwright/fly/AppTest.java
+++ src/test/java/com/github/miltonwright/fly/AppTest.java
@@ -1,5 +1,6 @@
 package com.github.miltonwright.fly;
 
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -7,8 +8,9 @@ import java.util.List;
 import org.eclipse.jetty.server.Server;
 
 import com.github.miltonwright.fly.App.Flight;
+import com.google.common.base.Charsets;
 import com.google.common.cache.CacheLoader;
-import com.google.common.collect.ImmutableList;
+import com.google.common.io.CharStreams;
 
 import junit.framework.TestCase;
 
@@ -17,7 +19,7 @@ public class AppTest extends TestCase {
     private static final CacheLoader<String, List<Flight>> FLIGHT_LOADER = new CacheLoader<String, List<Flight>>() {
         @Override
         public List<Flight> load(String arg) throws Exception {
-            return ImmutableList.of();
+            return App.unXml(AppTest.class.getResource("testdata.xml").openStream());
         }
     };
 
@@ -26,6 +28,8 @@ public class AppTest extends TestCase {
         URLConnection connection = new URL(server.getURI() + "flyavganger").openConnection();
         connection.connect();
         assertEquals("text/plain", connection.getHeaderField("Content-Type"));
+        String response = CharStreams.toString(new InputStreamReader(connection.getInputStream(), Charsets.UTF_8));
+        assertTrue(response, response.contains("AY2681\tAY\tD\t2018-11-08T21:25:00Z\tHOV\tA26\tDeparted at 22:29:00"));
         server.stop();
     }
 }
