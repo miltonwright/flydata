5d555105841f73cb5ca35dab7b35e4d9ac240264
Milton Wright <miltonwright@example.com>
2018-11-11 20:04:39 +0100

    Empty mvn project created with eclipse

---
+++ src/main/java/com/github/miltonwright/fly/App.java
@@ -0,0 +1,6 @@
+package com.github.miltonwright.fly;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/github/miltonwright/fly/AppTest.java
@@ -0,0 +1,10 @@
+package com.github.miltonwright.fly;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+    public void testApp() {
+        assertTrue( true );
+    }
+}
