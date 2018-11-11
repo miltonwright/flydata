44b9244224a20cdcd59d1f2ea7ba3eaf2cf2bc7c
Milton Wright <miltonwright@example.com>
2018-11-11 20:04:39 +0100

    Add jetty with basic boilerplate code

--- src/main/java/com/github/miltonwright/fly/App.java
+++ src/main/java/com/github/miltonwright/fly/App.java
@@ -1,6 +1,37 @@
 package com.github.miltonwright.fly;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
 public class App {
-    public static void main(String[] args) {
+    private static class Handler extends AbstractHandler {
+        @Override
+        public void handle(String path, Request request, HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException, ServletException {
+            if (!"/flyavganger".equals(path) || !"GET".equals(httpRequest.getMethod())) {
+                return;
+            }
+
+            httpResponse.setContentType("text/plain");
+            request.setHandled(true);
+        }
+    }
+
+    public static Server createServer(int port) throws Exception {
+        Server server = new Server(port);
+        server.setHandler(new Handler());
+        server.start();
+        return server;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = createServer(Integer.getInteger("com.github.miltonwright.fly.port", 8080));
+        server.join();
     }
 }
--- src/test/java/com/github/miltonwright/fly/AppTest.java
+++ src/test/java/com/github/miltonwright/fly/AppTest.java
@@ -1,10 +1,19 @@
 package com.github.miltonwright.fly;
 
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.eclipse.jetty.server.Server;
+
 import junit.framework.TestCase;
 
 public class AppTest extends TestCase {
 
-    public void testApp() {
-        assertTrue( true );
+    public void testApp() throws Exception {
+        Server server = App.createServer(0);
+        URLConnection connection = new URL(server.getURI() + "flyavganger").openConnection();
+        connection.connect();
+        assertEquals("text/plain", connection.getHeaderField("Content-Type"));
+        server.stop();
     }
 }
